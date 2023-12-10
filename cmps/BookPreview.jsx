8d459edc3137f21a954d9e207ca6a07c24d5f320
Amir Greenberg <amirgggg@gmail.com>
2023-12-10 19:41:51 +0200

    add pageCount feature

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -2,8 +2,16 @@ export function BookPreview({ book }) {
     return (
         <article className="book-preview">
             <h2>Title: {book.title}</h2>
-            <h4>Price: {book.listPrice.currencyCode}{book.listPrice.amount}</h4>
+            <h4>
+                Price: {book.listPrice.currencyCode}
+                {book.listPrice.amount}
+            </h4>
             <img src={`${book.thumbnail}`} />
+            {book.pageCount > 200 && book.pageCount < 500 && (
+                <h4>Descent Reading</h4>
+            )}
+            {book.pageCount > 500 && <h4>Serius Reading</h4>}
+            {book.pageCount < 100 && <h4>Light Reading</h4>}
         </article>
     )
 }
